e8795a613f0b4777f25491a9a39394337185239b
p88h <staszek@gmail.com>
2020-12-01 14:01:05 +0100

    split invocation / parsing

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,27 +1,25 @@
-import java.lang.NullPointerException
+import java.io.File
 import java.util.Scanner
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
+import kotlin.collections.set
+import com.google.common.io.Resources;
 
-fun part1() {
-    val reader = Scanner(System.`in`)
+fun part1(input: ArrayList<Int>) {
     val prev = HashSet<Int>();
-    while (reader.hasNextInt()) {
-        val num: Int = reader.nextInt();
+    for (num in input) {
         if (prev.contains(2020 - num)) {
             println(num * (2020 - num));
         }
         prev.add(num);
     }
-
 }
 
-fun main(args: Array<String>) {
-    val reader = Scanner(System.`in`)
+fun part2(input: ArrayList<Int>) {
     val prev = HashSet<Int>();
     val sums = HashMap<Int, Int>();
-    while (reader.hasNextInt()) {
-        val num: Int = reader.nextInt();
+    for (num in input) {
         val rem = 2020 - num;
         if (sums.containsKey(rem)) {
             val pa: Int = sums[rem] ?: throw NullPointerException();
@@ -33,4 +31,18 @@ fun main(args: Array<String>) {
         }
         prev.add(num);
     }
+}
+
+fun main(args: Array<String>) {
+    var inputFileName = Resources.getResource("day1.in").path;
+    if (!args.isEmpty()) {
+        inputFileName = args[0];
+    }
+    val reader = Scanner(File(inputFileName));
+    val input = ArrayList<Int>();
+    while (reader.hasNextInt()) {
+        input.add(reader.nextInt());
+    }
+    part1(input);
+    part2(input);
 }
