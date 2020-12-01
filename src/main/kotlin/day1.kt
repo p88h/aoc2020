28d81c8df729a47a4395d36ab3acc824a6ad166a
Staszek Pasko <paskos@amazon.com>
2020-12-01 08:27:27 +0100

    Day 1 done

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,3 +1,36 @@
+import java.lang.NullPointerException
+import java.util.Scanner
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
+
+fun part1() {
+    val reader = Scanner(System.`in`)
+    val prev = HashSet<Int>();
+    while (reader.hasNextInt()) {
+        val num: Int = reader.nextInt();
+        if (prev.contains(2020 - num)) {
+            println(num * (2020 - num));
+        }
+        prev.add(num);
+    }
+
+}
+
 fun main(args: Array<String>) {
-    println("Day1!")
+    val reader = Scanner(System.`in`)
+    val prev = HashSet<Int>();
+    val sums = HashMap<Int, Int>();
+    while (reader.hasNextInt()) {
+        val num: Int = reader.nextInt();
+        val rem = 2020 - num;
+        if (sums.containsKey(rem)) {
+            val pa: Int = sums[rem] ?: throw NullPointerException();
+            val pb: Int = rem - pa;
+            println(pa * pb * num);
+        }
+        for (other in prev) {
+            sums[num + other] = num;
+        }
+        prev.add(num);
+    }
 }
