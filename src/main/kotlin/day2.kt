5d1f8a5ea9732b6acec4622e161ac079a965f7b6
Staszek Pasko <staszek@gmail.com>
2020-12-02 09:16:31 +0100

    streams and predicates

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -1,35 +1,28 @@
 import com.google.common.io.Resources
-import java.io.File
-import java.util.Scanner
+import java.io.BufferedReader
+import java.io.FileReader
 
-val regex = "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)".toRegex();
+internal val regex = "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)".toRegex()
+
+internal data class Password(val min: Int, val max: Int, val letter: Char, val text: String) {
+    fun pred1(): Boolean {
+        val cnt: Int = text.count { it.equals(letter) }
+        return cnt in min..max
+    }
+    fun pred2(): Boolean {
+        val first = text.length >= min && text[min - 1] == letter
+        val second = text.length >= max && text[max - 1] == letter
+        return first xor second
+    }
+}
 
 fun main(args: Array<String>) {
-    var inputFileName = Resources.getResource("day2.in").path;
-    if (!args.isEmpty()) {
-        inputFileName = args[0];
-    }
-    val reader = Scanner(File(inputFileName));
-    var total1 = 0;
-    var total2 = 0;
-    while (reader.hasNextLine()) {
-        regex.matchEntire(reader.nextLine())
-            ?.destructured
-            ?.let { (mins, maxs, letters, text) ->
-                val min = mins.toInt();
-                val max = maxs.toInt();
-                val letter = letters[0];
-                val cnt:Int = text.count{it.equals(letter)};
-                if (cnt in min..max) {
-                    total1 += 1;
-                }
-                val first = text.length >= min && text[min-1] == letter;
-                val second = text.length >= max && text[max-1] == letter;
-                if (first xor second) {
-                    total2 += 1;
-                }
-            }
-    }
-    println(total1);
-    println(total2);
+    var inputFileName = if (args.isEmpty()) Resources.getResource("day2.in").path else args[0]
+    val inputs = BufferedReader(FileReader(inputFileName)).lineSequence().map {
+        regex.matchEntire(it)?.destructured?.let { (mins, maxs, letters, text) ->
+            Password(mins.toInt(), maxs.toInt(), letters[0], text)
+        }
+    }.toList()
+    println(inputs.count { it!!.pred1() })
+    println(inputs.count { it!!.pred2() })
 }
