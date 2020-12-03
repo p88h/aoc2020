50f686ec1f356a502df4e9bbe218bb9436e9bc03
Staszek Pasko <staszek@gmail.com>
2020-12-03 09:20:28 +0100

    Part 2

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -1,24 +1,26 @@
 import com.google.common.io.Resources
 import java.io.BufferedReader
-import java.io.File
 import java.io.FileReader
-import java.util.*
+
+internal data class Point(var x: Int, var y: Int)
+
+internal fun tobogan(area: List<CharArray>, dir: Point): Int {
+    var pos = Point(0, 0)
+    var count = 0
+    while (pos.y < area.size) {
+        if (area[pos.y][pos.x] == '#') {
+            count += 1
+        }
+        pos.x = (pos.x + dir.x) % area[pos.y].size
+        pos.y += dir.y
+    }
+    println("slope ${dir}: ${count}")
+    return count
+}
 
 fun main(args: Array<String>) {
     val inputFileName = if (args.isEmpty()) Resources.getResource("day3.in").path else args[0]
-    val scanner = Scanner(File(inputFileName));
-    var count = 0;
-    var pos = 0;
-    while (scanner.hasNextLine()) {
-        var line = scanner.nextLine().toCharArray();
-        if (line[pos] == '.') {
-            line[pos] = 'O';
-        } else {
-            line[pos] = 'X';
-            count += 1;
-        }
-        println(String(line));
-        pos = (pos + 3) % line.size;
-    }
-    println(count);
+    val area = BufferedReader(FileReader(inputFileName)).lineSequence().map { it.toCharArray() }.toList()
+    val dirs = arrayOf(Point(1,1), Point(3,1), Point(5,1,), Point(7,1 ), Point( 1, 2))
+    println(dirs.fold(1) {s, it -> s * tobogan(area,it)})
 }
