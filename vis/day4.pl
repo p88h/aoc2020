41754371352a28d9419bce032dd936d1677f5a98
Staszek Pasko <staszek@gmail.com>
2020-12-04 16:59:43 +0100

    Faces, Please

--- vis/day4.pl
+++ vis/day4.pl
@@ -5,6 +5,21 @@ use Time::HiRes qw(usleep);
 setmode(1);
 cls;
 
+@hair = ('/-----\\', '.^^^^^.', '.!!!!!.', '///-\\\\\\', '#######', '/~~~~~\\');
+@brow = ('| _ _ |', '|^- -^|', '| ~ ~ |', '| = = |', '| - - |');
+@eyes = ('|     |', '|[] []|', '| o o |', '| O O |', '| @ @ |', '| x x |');
+@nose = ('|  ^  |', '|  V  |', '|  &  |', '|  *  |', '|  v  |');
+@lips = (' \ o / ', ' \ O / ', ' \ - / ', ' \ ~ / ', ' \[-]/ ');
+@neck = (' | . | ', '  | |  ', ' / $ \ ', ' /^w^\  ',' {-+-} ');
+
+$ecm{amb} = 'cyan';
+$ecm{blu} = 'blue';
+$ecm{brn} = 'red';
+$ecm{gry} = 'white';
+$ecm{grn} = 'green';
+$ecm{hzl} = 'yellow';
+$ecm{oth} = 'magenta';
+
 sub display {
   my ($p, $status, $count) = @_;
   locate(2,1);savepos;
@@ -28,8 +43,8 @@ sub display {
   |                   |    EYES : @<<<<<<< |
                               $p->{ecl}
   |                   |                    |
-  |   [ @|||||||| ]   |                    |
-        $p->{cid} or "???"
+  |                   |    [ @|||||||| ]   |
+                             $p->{cid} or "???"
   \----------------------------------------/
 
   VALID: @<<<<<<
@@ -37,13 +52,63 @@ sub display {
 .
   write;
   print "\n               ";
+
+  if ($p->{byr}) {
+    $age = 2020-int($p->{byr});
+    if ($age < 1) {
+      $h = 0;
+    } elsif ($age < 10) {
+      $h = 1;
+    } elsif ($age < 25) {
+      $h = 2;
+    } elsif ($age < 80) {
+      $h = 3;
+    } else {
+      $h = 5;
+    }
+  } else {
+    $h = 4;
+  }
+
+  if ($p->{ecl}) {
+    if ($age > 100) {
+      $e = 5;
+    } else {
+      $e = 2+int(rand(3));
+    }
+    $ec = $ecm{$p->{ecl}};
+  } else {
+    $e = 0;
+    $ec = 'clear';
+  }
+  if ($p->{hcl} =~ /([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/) {
+    $r = ord(pack("H*",$1));
+    $g = ord(pack("H*",$2));
+    $b = ord(pack("H*",$3));
+    $hc = "\x1b[38;2;"."$r;$g;$b"."m".$hair[$h]."\x1b[0m\n";
+  } else {
+    $hc = $hair[$h];
+  }
+  print locate(6,10),$hc;
+  color "reset";
+  print locate(7,10),$brow[int(rand(4))];
+  locate(8,10);
+  print "|";
+  color $ec;
+  print substr($eyes[$e],1,5);
+  color "reset";
+  print "|";
+  print locate(9,10),$nose[int(rand(4))];
+  print locate(10,10),$lips[int(rand(4))];
+  print locate(11,10),$neck[int(rand(4))];
+
   if ($status eq "APPROVED") {
      $color = "green";
   } else { 
      $color = "red";
   }
   for $l (0..length($status)) {
-    locate(8,10);
+    locate(13,10);
     color $color;
     print substr($status,0,$l),"\n";
     locate(18,1);
