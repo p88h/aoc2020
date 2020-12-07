e40b0b263d1edf74f7cd1b93fcdcb084bb535819
Staszek Pasko <staszek@gmail.com>
2020-12-07 09:45:21 +0100

    a bit nicer maybe

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -1,45 +1,54 @@
 import com.google.common.io.Resources
 import java.io.File
 
-internal data class Bag(val name: String) {
-    var total = 0L
-    var contents = ArrayList<Pair<String, Int>>()
+internal data class Graph(val start: String) {
+    var contents = HashMap<String, ArrayList<Pair<String, Int>>>()
+    var inverted = HashMap<String, ArrayList<String>>()
+    var totals = HashMap<String, Long>()
 
-    fun compute(graph: HashMap<String, Bag>): Long {
-        if (total == 0L) contents.forEach {
-            val inner = graph[it.first]
-            total += it.second.toLong() * inner!!.compute(graph)
+    // BFS on inverted graph, return count of visited nodes
+    fun search(from: String = start): Int {
+        var visited = hashSetOf(from)
+        var queue = ArrayDeque<String>()
+        queue.add(from)
+        while (queue.isNotEmpty()) {
+            val inner = queue.removeFirst()
+            inverted[inner]?.forEach {
+                if (!visited.contains(it)) {
+                    queue.add(it)
+                    visited.add(it)
+                }
+            }
         }
-        return total + 1
+        return visited.size
+    }
+
+    // DFS on contents graph, return number of bags in subtree
+    fun compute(from: String = start): Long {
+        if (!totals.containsKey(from)) {
+            var total = 0L
+            contents[from]!!.forEach {
+                total += it.second.toLong() * compute(it.first)
+            }
+            totals[from] = total
+        }
+        return totals[from]!! + 1
     }
 }
 
 fun main(args: Array<String>) {
     val inputFileName = if (args.isEmpty()) Resources.getResource("day7.in").path else args[0]
-    var graph = HashMap<String, Bag>()
-    var inverted = HashMap<String, ArrayList<String>>()
+    var graph = Graph("shiny gold")
     File(inputFileName).bufferedReader().readLines().forEach {
         val tokens = it.split(" ")
         val outer = tokens[0] + " " + tokens[1];
-        graph[outer] = Bag(outer);
+        graph.contents[outer] = ArrayList()
         for (idx in 1 until tokens.size / 4) {
             val inner = tokens[4 * idx + 1] + " " + tokens[4 * idx + 2]
-            graph[outer]!!.contents.add(inner to tokens[4 * idx].toInt())
-            inverted.getOrPut(inner, { ArrayList() }).add(outer)
+            graph.contents[outer]!!.add(inner to tokens[4 * idx].toInt())
+            graph.inverted.getOrPut(inner, { ArrayList() }).add(outer)
         }
     }
-    var visited = hashSetOf("shiny gold")
-    var queue = ArrayDeque<String>()
-    queue.add("shiny gold")
-    while (queue.isNotEmpty()) {
-        val inner = queue.removeFirst()
-        inverted[inner]?.forEach {
-            if (!visited.contains(it)) {
-                queue.add(it)
-                visited.add(it)
-            }
-        }
-    }
-    println(visited.size - 1)
-    println(graph["shiny gold"]!!.compute(graph) - 1)
+    println(graph.search() - 1)
+    println(graph.compute() - 1)
 }
