dba09bcdc279b2b23a5db7f8029d833d508f943b
Staszek Pasko <staszek@gmail.com>
2020-12-09 08:57:59 +0100

    a bit nicer python for day9

--- other/day9.py
+++ other/day9.py
@@ -1,28 +1,27 @@
 import sys
+from itertools import combinations
+
 nums = []
 runs = []
-preamble = 25
-invalid = 0
-# part 1
+
+# part 1, O(N*P**2)
+q = 0
 for line in sys.stdin:
-    a = int(line)
-    l = len(nums)
-    if invalid == 0 and l >= preamble:
-        found = False
-        for b in range(preamble):
-            for c in range(b):
-                if a == nums[l - b - 1] + nums[l - c -1]:
-                    found = True
-        if not found:
-            invalid = a
-            print(a)
+    q = int(line)
+    if len(nums) >= 25:
+        if not any(q == b + c for (b, c) in combinations(nums[-25:], 2)):
+            print(q)
             break
-    nums.append(a)
-# part 2
-for a in range(len(nums)):
-    for r in range(len(runs)):
-        runs[r] += nums[a]
-        if runs[r] == invalid:
-            rng = nums[r:a]
-            print("sum({}) = {}\n{}\n".format(rng,invalid,min(rng)+max(rng)))
-    runs.append(nums[a])
+    nums.append(q)
+
+# part 2, O(N-ish)
+t = 0
+i = 0
+for j in range(len(nums)):
+    t += nums[j]
+    while i <= j and t > q:
+        t -= nums[i]
+        i += 1
+    if t == q:
+        rng = nums[i:j]
+        print("sum({}) = {}\n{}\n".format(rng, q, min(rng)+max(rng)))
