036dded9430b4223bdb76e710851d3c7f0f46f6a
Staszek Pasko <staszek@gmail.com>
2020-12-09 11:41:13 +0100

    Day 9

---
+++ src/main/kotlin/day9.kt
@@ -0,0 +1,55 @@
+import com.google.common.io.Resources
+import java.io.File
+
+// O(N * P)
+fun part1(nums: List<Long>): Long {
+    var sums = HashMap<Long, Long>()
+    for (a in nums.indices) {
+        val b = a - 25
+        if (b >= 0 && nums[a]!! !in sums) {
+            println(nums[a])
+            return nums[a]
+        }
+        for (c in b + 1 until a) {
+            if (b >= 0) {
+                // remove (b+c)
+                val s1 = nums[b] + nums[c]
+                sums.merge(s1, -1, Long::plus)
+                // cleanup
+                if (sums.getValue(s1) == 0L) {
+                    sums.remove(s1)
+                }
+            }
+            if (c >= 0) {
+                // add (c+a)
+                val s2 = nums[c] + nums[a]
+                sums.merge(s2, 1, Long::plus)
+            }
+        }
+    }
+    return 0L
+}
+
+// O(N)
+fun part2(nums: List<Long>, q: Long): Long {
+    var tot = 0L
+    var i = 0
+    for (j in nums.indices) {
+        tot += nums[j]
+        while (i <= j && tot > q) {
+            tot -= nums[i]
+            i += 1
+        }
+        if (tot == q) {
+            val found = nums.subList(i, j)
+            return found.min()!! + found.max()!!
+        }
+    }
+    return 0L
+}
+
+fun main(args: Array<String>) {
+    val inputFileName = if (args.isEmpty()) Resources.getResource("day9.in").path else args[0]
+    val nums = File(inputFileName).bufferedReader().readLines().map { it.toLong() }.toList()
+    println(part2(nums, part1(nums)))
+}
