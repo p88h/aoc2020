358155f1e7e86da50ed5745e8fe61f7d191146be
p88h <staszek@gmail.com>
2020-12-11 09:28:10 +0100

    Refactor Day11 into something readable.

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,112 +1,52 @@
 import com.google.common.io.Resources
 import java.io.File
 
+internal data class Seat(val pos: Pair<Int, Int>) {
+    var adj = ArrayList<Pair<Int, Int>>()
+    var nc = 0
+
+    fun InitAdj(area: List<CharArray>, maxd: Int) {
+        adj.clear()
+        for (ii in -1..1) for (jj in -1..1) {
+            if (ii == 0 && jj == 0) continue
+            for (k in 1..maxd) {
+                val i = pos.first + k * ii
+                val j = pos.second + k * jj
+                if (!(i >= 0 && i < area.size && j >= 0 && j < area[i].size)) break
+                if (area[i][j] != '.') {
+                    adj.add(i to j)
+                    break
+                }
+            }
+        }
+    }
+
+    fun ComputeNC(area: List<CharArray>) {
+        nc = adj.count { area[it.first][it.second] == '#' }
+    }
+
+    fun Update(area: List<CharArray>, maxc: Int): Boolean {
+        val p = area[pos.first][pos.second]
+        if (nc == 0) area[pos.first][pos.second] = '#'
+        if (nc >= maxc) area[pos.first][pos.second] = 'L'
+        return area[pos.first][pos.second] != p
+    }
+}
+
 fun main(args: Array<String>) {
     val inputFileName = if (args.isEmpty()) Resources.getResource("day11.in").path else args[0]
     var area = File(inputFileName).bufferedReader().lineSequence().map { it.toCharArray() }.toList()
-    while (true) {
-        var mod = 0
-        var cnt = 0
-        for (i in area.indices) {
-            for (j in area[i].indices) {
-                var occ = 0;
-                for (ii in -1..1) {
-                    if (i + ii >= 0 && i + ii < area.size) for (jj in -1..1) {
-                        if (j + jj >= 0 && j + jj < area[i].size) {
-                            if (ii == 0 && jj == 0) continue
-                            if (area[i+ii][j+jj] == '#' || area[i+ii][j+jj] == '@') occ += 1;
-                        }
-                    }
-                }
-                if (area[i][j]=='L' && occ == 0) {
-                    area[i][j] = '*'
-                }
-                if (area[i][j]=='#' && occ >= 4) {
-                    area[i][j] = '@'
-                }
-            }
-        }
-        for (i in area.indices) {
-            for (j in area[i].indices) {
-                if (area[i][j]=='*') {
-                    area[i][j] = '#'
-                    mod += 1
-                }
-                if (area[i][j]=='@') {
-                    area[i][j] = 'L'
-                    mod += 1
-                }
-                if (area[i][j] == '#') {
-                    cnt += 1
-                }
-            }
-            //println(area[i])
-        }
-        println(cnt)
-        if (mod == 0) {
-            break
-        }
-    }
-
-    for (i in area.indices) {
-        for (j in area[i].indices) {
-            if (area[i][j] != '.') {
-                area[i][j] = 'L'
-            }
-        }
-    }
-
-    while (true) {
-        var mod = 0
-        var cnt = 0
-        for (i in area.indices) {
-            for (j in area[i].indices) {
-                var occ = 0;
-                for (ii in -1..1) {
-                    for (jj in -1..1) {
-                        if (ii == 0 && jj == 0) continue
-                        var k=1
-                        while (i + k*ii >= 0 && i + k*ii < area.size && j + k*jj >= 0 && j + k*jj < area[i].size) {
-                            if (area[i+k*ii][j+k*jj] == '#' || area[i+k*ii][j+k*jj] == '@') {
-                                occ += 1
-                                break
-                            }
-                            if (area[i+k*ii][j+k*jj] == 'L' || area[i+k*ii][j+k*jj] == '*') {
-                                break
-                            }
-                            k+=1
-                        }
-                    }
-                }
-                if (area[i][j]=='L' && occ == 0) {
-                    area[i][j] = '*'
-                }
-                if (area[i][j]=='#' && occ >= 5) {
-                    area[i][j] = '@'
-                }
-            }
-        }
-        for (i in area.indices) {
-            for (j in area[i].indices) {
-                if (area[i][j]=='*') {
-                    area[i][j] = '#'
-                    mod += 1
-                }
-                if (area[i][j]=='@') {
-                    area[i][j] = 'L'
-                    mod += 1
-                }
-                if (area[i][j] == '#') {
-                    cnt += 1
-                }
-            }
-            //println(area[i])
-        }
-        println(cnt)
-        //Thread.sleep(1000)
-        if (mod == 0) {
-            break
-        }
-    }
-
+    var seats = ArrayList<Seat>()
+    for (i in area.indices) for (j in area[i].indices) if (area[i][j] != '.') seats.add(Seat(i to j))
+    seats.forEach { it.InitAdj(area, 1) }
+    do {
+        seats.forEach { it.ComputeNC(area) }
+    } while (seats.count { it.Update(area, 4) } > 0)
+    println(seats.count { area[it.pos.first][it.pos.second] == '#' });
+    seats.forEach { it.Update(area, 0) }
+    seats.forEach { it.InitAdj(area, 999999) }
+    do {
+        seats.forEach { it.ComputeNC(area) }
+    } while (seats.count { it.Update(area, 5) } > 0)
+    println(seats.count { area[it.pos.first][it.pos.second] == '#' });
 }
