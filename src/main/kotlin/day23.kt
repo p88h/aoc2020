d4a2c2a24bdb465f12624d647ce94e960024b060
Staszek Pasko <staszek@gmail.com>
2020-12-24 14:25:22 +0100

    Simplified Day 23 (table-based)

--- src/main/kotlin/day23.kt
+++ src/main/kotlin/day23.kt
@@ -1,48 +1,40 @@
-internal data class Cup(val label: Int, var next: Cup? = null)
-
-internal fun game(nums: List<Int>, iter: Int) : Cup {
-    var cups = ArrayList(nums.map { Cup(it) })
-    cups.zipWithNext { a, b -> a.next = b }
-    cups.last().next = cups.first()
-    var cur = cups.first()
-    for (i in 1..nums.size) {
-        cups[cur.label - 1] = cur
-        cur = cur.next!!
-    }
+internal fun game(nums: List<Int>, iter: Int) : IntArray {
+    var next = IntArray(nums.size + 1)
+    nums.zipWithNext { a, b -> next[a] = b }
+    next[nums.last()] = nums.first()
+    var cur = nums.first()
     for (i in 1..iter) {
         var tail = cur
-        var head = cur.next
-        var used = hashSetOf(cur.label)
+        var head = next[cur]
+        var used = hashSetOf(cur)
         for (j in 1..3) {
-            tail = tail.next!!
-            used.add(tail.label)
+            tail = next[tail]
+            used.add(tail)
         }
-        var d = cur.label
-        while (used.contains(d)) d = if (d > 1) d - 1 else nums.size
-        var dst = cups[d - 1]
-        cur.next = tail.next
-        tail.next = dst.next
-        dst.next = head
-        cur = cur.next
+        var dst = cur
+        while (used.contains(dst)) dst = if (dst > 1) dst - 1 else nums.size
+        next[cur] = next[tail]
+        next[tail] = next[dst]
+        next[dst] = head
+        cur = next[cur]
     }
-    while (cur.label != 1) cur = cur.next!!
-    return cur
+    return next
 }
 
-internal fun CupsString(from: Cup, count: Int = 9): String {
+internal fun walk(next: IntArray, count: Int = 9): String {
     var ret = StringBuilder()
-    var cur = from
+    var cur = 1
     for (i in 1..count) {
-        ret.append(cur.label)
-        cur = cur.next!!
+        ret.append(cur)
+        cur = next[cur]
     }
     return ret.toString()
 }
 
 fun main() {
     var input = ArrayList(readLine()!!.map { it.toString().toInt() })
-    println(CupsString(game(input, 100)))
+    println(walk(game(input, 100)))
     input.addAll(10..1000000)
     val res = game(input, 10000000)
-    println(res.next!!.label.toLong() * res.next!!.next!!.label.toLong())
+    println(res[1].toLong() * res[res[1]].toLong())
 }
